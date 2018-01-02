6950f1b20c121b76da118ce73c831c3382aeca9c
acalhoon <a.calhoon@gmail.com>
2018-01-02 12:53:14 -0500

    Refactor: eliminate ReaderT and make workers more generic

--- b2c.hs
+++ b2c.hs
@@ -3,7 +3,7 @@
 {- |
  - Program     : b2c
  - Description : Converts input into a C array.
- - Copyright   : (c) A.Calhoon, 2017
+ - Copyright   : (c) A.Calhoon, 2017-2018
  - License     : BSD3
  - Maintainer  : a.calhoon@gmail.com
  - Stability   : experimental
@@ -11,22 +11,20 @@
  - 
  - Converts input sources into a C file containing an array of uint8_t bytes
  - and an H file with the necessary defines and externs so that array can be
- - referenced from other program files.
+ - referenced from C and C++ program files.
  -}
 --------------------------------------------------------------------------------
 module Main where
-import           Control.Arrow              ((&&&))
 import           Control.Exception          (IOException, try)
-import           Control.Monad              (foldM, (>=>), void)
+import           Control.Monad              (foldM, void)
 import           Control.Monad.IO.Class     (liftIO)
-import           Control.Monad.Trans.Reader (ReaderT, runReaderT, ask, asks)
 import           Control.Monad.Trans.State  (execStateT, modify')
 import           Data.Char                  (toUpper)
 import           Data.List.Split            (chunksOf)
 import           Data.Monoid                ((<>))
 import qualified Options.Applicative        as OA
 import           System.Directory           (removeFile)
-import qualified System.IO                  as IO
+import           System.IO
 import           Text.Printf                (printf)
 
 --------------------------------------------------------------------------------
@@ -47,18 +45,6 @@ data Options =
   , source    :: InputSource -- ^ Where to retrieve input from.
   , chunkSize :: Int         -- ^ Maximum number of array bytes per line.
   }
-
---------------------------------------------------------------------------------
--- | Environment for creating files.
-data Env =
-  Env
-  { handle  :: IO.Handle -- ^ Output file handle.
-  , options :: Options   -- ^ Command line options
-  }
-
---------------------------------------------------------------------------------
--- | Environment Reader in IO.
-type EnvReader = ReaderT Env IO
 
 --------------------------------------------------------------------------------
 -- | Build the C file name from command line args.
@@ -147,9 +133,8 @@ tryIOE :: IO a -> IO (Either IOException a)
 tryIOE = try
 
 --------------------------------------------------------------------------------
--- | Turn command line arguments into the target C and H files.
-b2c :: Options -- ^ command line arguments
-    -> IO()
+-- | Turn command line arguments @opt@ into the target C and H files.
+b2c :: Options -> IO ()
 b2c opt = tryIOE (createCFile opt >>= createHFile opt) >>= cleanUp
   where cleanUp (Left exc) = do tryDelete (cFileName opt)
                                 tryDelete (hFileName opt)
@@ -158,94 +143,93 @@ b2c opt = tryIOE (createCFile opt >>= createHFile opt) >>= cleanUp
         tryDelete name = void $ tryIOE $ removeFile name
 
 --------------------------------------------------------------------------------
--- | Creates the target C file.
-createCFile :: Options      -- ^ command line arguments
+-- | Creates the target C file from @opts@.
+createCFile :: Options
             -> IO CArrayLen -- ^ total length of input read
-createCFile o = withOutputFile (cFileName o) o writeCFile 
+createCFile opts = withFile (cFileName opts) WriteMode $ writeCFile opts
 
 --------------------------------------------------------------------------------
--- | Perform @m@ on @file@ with that file open for reading in binary mode.
-withOutputFile :: FilePath    -- ^ file to be created
-               -> Options     -- ^ command line arguments
-               -> EnvReader a -- ^ computation to perform within an Env
-               -> IO a        -- ^ result of performed action
-withOutputFile name o ma =
-  IO.withFile name IO.WriteMode $ \h ->
-  runReaderT ma Env { handle=h, options=o }
+-- | Writes @outfile@ as a C file containing an include, array definition, and
+-- array contents derived from @opts@ and returns the total length of the C
+-- array.
+writeCFile :: Options
+           -> Handle       -- ^ output file handle
+           -> IO CArrayLen -- ^ total length of the C file array
+writeCFile opts outfile = do
+  hPutStr outfile . unlines $ [ "#include \"" ++ hFileName opts ++ "\""
+                              , ""
+                              , "const uint8_t " ++ varName opts ++ "[] = {" ]
 
---------------------------------------------------------------------------------
--- | Writes the C file with include, array definition and array contents.
-writeCFile :: EnvReader CArrayLen -- ^ total length of input read
-writeCFile = do
-  (h,o) <- asks $ handle &&& options
-  liftIO $ do IO.hPutStrLn h $ "#include \"" ++ hFileName o ++ "\""
-              IO.hPutStrLn h $ ""
-              IO.hPutStrLn h $ "const uint8_t " ++ varName o ++ "[] = {"
-  total <- sourcePutArray $ source o
-  liftIO $    IO.hPutStrLn h $ "};"
+  let maxbytes = chunkSize opts
+  total <- case source opts of
+    (InFiles fs) -> let sum' !acc f = (+acc) <$> putFileArray outfile f maxbytes
+                    in foldM sum' 0 fs
+    Stdin        -> putArray outfile maxbytes stdin
+
+  hPutStrLn outfile $ "};"
   return total
 
 --------------------------------------------------------------------------------
--- | Writes the contents of all of the input sources to the output file as
--- array contents.
-sourcePutArray :: InputSource         -- ^ input data source
-               -> EnvReader CArrayLen -- ^ total length of input read
-sourcePutArray Stdin = liftIO (IO.hSetBinaryMode IO.stdin True >> IO.getContents) >>= putArray
-sourcePutArray (InFiles fs) = foldM (\ !acc name -> (+acc) <$> filePutArray name) 0 fs
+-- | Writes the contents of @infile@ to @outfile@ as the lines of an array. The
+-- maximum number of bytes in a given line is @maxbytes@.
+putArray :: Handle       -- ^ output file handle
+         -> Int          -- ^ maximum number of bytes per line
+         -> Handle       -- ^ input file handle
+         -> IO CArrayLen -- ^ total length of input read from @infile@
+putArray outfile maxbytes infile =
+  getArrayBytes infile maxbytes >>= putArrayBytes outfile
 
 --------------------------------------------------------------------------------
--- | Writes the contents of a named input file to the output file as array
--- contents.  The array contents are pre- and post-fixed with comments
--- including the input file's full path.
-filePutArray :: FilePath            -- ^ input file path
-             -> EnvReader CArrayLen -- ^ length of input read from the input file
-filePutArray name = ask >>= liftIO . putLines
-  where putLines env = do 
-          let h = handle env
-          IO.hPutStrLn h $ "  /* -- Start of File: \"" ++ name ++ "\" -- */"
-          len <- withInputFile name $ \s -> runReaderT (putArray s) env
-          IO.hPutStrLn h $ "  /* -- End of File: \"" ++ name ++ "\" -- */"
-          return len
+-- | Read binary input from @infile@ in chunks of length @maxbytes@.
+getArrayBytes :: Handle      -- ^ input file handle
+              -> Int         -- ^ length of input chunks
+              -> IO [String] -- ^ byte-string chunks
+getArrayBytes infile maxbytes = do
+  hSetBinaryMode infile True
+  chunksOf maxbytes <$> hGetContents infile
 
 --------------------------------------------------------------------------------
--- | Perform @action@ on @name@ with that file open for reading in binary mode.
-withInputFile :: FilePath         -- ^ file to be read
-              -> (String -> IO a) -- ^ action to perform on file contents
-              -> IO a
-withInputFile name action =
-  IO.withBinaryFile name IO.ReadMode $ IO.hGetContents >=> action
+-- | Writes to @outfile@ each byte-string in @xs@ as array lines of uint8_t
+-- bytes.
+putArrayBytes :: Handle       -- ^ output file handle
+              -> [String]     -- ^ byte-strings
+              -> IO CArrayLen -- ^ total number of bytes written
+putArrayBytes outfile xs = do
+  let writeCount x = modify' (+ length x) >> liftIO (putLine x)
+  execStateT (mapM_ writeCount xs) 0
+  where putLine = hPutStrLn outfile . ("  "++) . concatMap (printf "0x%02X,")
 
 --------------------------------------------------------------------------------
--- | Writes the contents of an input file to the output file as array contents
--- in @maxChunkSize@ byte chunks.
-putArray :: String              -- ^ file contents
-         -> EnvReader CArrayLen -- ^ length of input read from the input file
-putArray s = do
-  size <- asks (chunkSize . options)
-  h <- asks handle
-  let writeCount l = modify' (+ length l) >> liftIO (putAsArrayLn h l)
-  execStateT (mapM_ writeCount $ chunksOf size s) 0
-
---------------------------------------------------------------------------------
--- | Writes a string of bytes to @handle@ as a line of uint8_t array bytes
-putAsArrayLn :: IO.Handle -> String -> IO ()
-putAsArrayLn h = IO.hPutStrLn h . ("  "++) . concatMap (printf "0x%02X,")
+-- | Writes to @outfile@ the contents of the file at path @inpath@ as an array
+-- contents. The maximum number of bytes on each line of the array is
+-- @maxbytes@.  The array contents are pre- and post-fixed with comments
+-- including the @inpath@.
+putFileArray :: Handle       -- ^ output file handle
+             -> FilePath     -- ^ input file path
+             -> Int          -- ^ maximum number of bytes for an array line
+             -> IO CArrayLen -- ^ total length of input read from @infile@
+putFileArray outfile inpath maxbytes = do
+  hPutStrLn outfile $ "  /* -- Start of File: \"" ++ inpath ++ "\" -- */"
+  total <- withFile inpath ReadMode $ putArray outfile maxbytes
+  hPutStrLn outfile $ "  /* -- End of File: \"" ++ inpath ++ "\" -- */"
+  return total
 
 --------------------------------------------------------------------------------
 -- | Creates the target H file.
-createHFile :: Options   -- ^ command line arguments
+createHFile :: Options
             -> CArrayLen -- ^ length of the C array
             -> IO ()
-createHFile o len = withOutputFile (hFileName o) o $ writeHFile len
+createHFile opts len =
+  withFile (hFileName opts) WriteMode $ writeHFile opts len
 
 --------------------------------------------------------------------------------
--- | Write the H file include guards, length #define, extern variable name
--- reference, and a trailing include guard.
-writeHFile :: CArrayLen    -- ^ total length of the array
-           -> EnvReader ()
-writeHFile len = do
-  (h,o) <- asks (handle &&& options)
-  liftIO $ IO.hPutStr h . unlines $ getHFileLines o len
+-- | Writes @outfile@ as an H file containing include guards, length #define,
+-- and extern reference to the C array.
+writeHFile :: Options
+           -> CArrayLen    -- ^ length of the array
+           -> Handle       -- ^ output file handle
+           -> IO ()
+writeHFile opts len outfile = hPutStr outfile . unlines $ getHFileLines opts len
 
 --------------------------------------------------------------------------------
 -- | A list of lines to create the H file.
@@ -263,12 +247,13 @@ getHFileLines opt len =
   , "#include <stdint.h>"
   , ""
   , "#define " ++ lengthDef ++ " (uint32_t)(" ++ show len ++ "UL)"
-  , "extern const uint8_t " ++ varName opt ++ "["++ lengthDef ++ "];"
+  , "extern const uint8_t " ++ var ++ "["++ lengthDef ++ "];"
   , ""
   , "#ifdef __cplusplus"
   , "}"
   , "#endif /* _cplusplus */"
   , ""
   , "#endif /* " ++ guardDef ++ " */"
-  ] where guardDef  = map toUpper (fileName opt) ++ "_H"
-          lengthDef = map toUpper (varName opt) ++ "_LEN"
+  ] where var       = varName opt
+          guardDef  = map toUpper (fileName opt) ++ "_H"
+          lengthDef = map toUpper var ++ "_LEN"
